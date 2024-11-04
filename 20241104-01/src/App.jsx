7d5ae98f4d55230209cd63947f1311de0193400b
Yoon Hyungseok <yhsphd05@gmail.com>
2024-11-04 20:28:30 +0900

    20241104 수업

---
+++ 20241104-01/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import MovingDot from "./MovingDot";
+import { ImmerTest } from "./ImmerTest";
+import ListTest from "./ListTest";
+import ShapeEditor from "./ShapeEditor";
+
+function App() {
+  return (
+    <>
+      <MovingDot></MovingDot>
+      <ImmerTest></ImmerTest>
+      <ListTest></ListTest>
+      <ShapeEditor></ShapeEditor>
+    </>
+  );
+}
+
+export default App;
---
+++ 20241104-01/src/ShapeEditor.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+let initialShapes = [
+  { id: 0, type: "circle", x: 50, y: 100 },
+  { id: 1, type: "square", x: 150, y: 100 },
+  { id: 2, type: "circle", x: 250, y: 100 },
+];
+
+export default function ShapeEditor() {
+  const [shapes, setShapes] = useState(initialShapes);
+
+  function handleClick() {
+    const nextShapes = shapes.map((shape) => {
+      if (shape.type === "square") {
+        // 변경시키지 않고 반환합니다.
+        return shape;
+      } else {
+        // 50px 아래로 이동한 새로운 원을 반환합니다.
+        return {
+          ...shape,
+          y: shape.y + 50,
+        };
+      }
+    });
+    // 새로운 배열로 리렌더링합니다.
+    setShapes(nextShapes);
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>Move circles down!</button>
+      {shapes.map((shape) => (
+        <div
+          style={{
+            background: "purple",
+            position: "absolute",
+            left: shape.x,
+            top: shape.y,
+            borderRadius: shape.type === "circle" ? "50%" : "",
+            width: 20,
+            height: 20,
+          }}
+        />
+      ))}
+    </>
+  );
+}

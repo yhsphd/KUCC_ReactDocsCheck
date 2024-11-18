8efb149ed34df3c825569e753bf14ce1dcc582e4
Yoon Hyungseok <yhsphd05@gmail.com>
2024-11-18 20:30:23 +0900

    20241118 수업

---
+++ 20241118-01/src/App.jsx
@@ -0,0 +1,11 @@
+import StyledTest1 from "./StyledTest1";
+import StyledTest2 from "./StyledTest2";
+
+export default function App() {
+  return (
+    <>
+      <StyledTest1></StyledTest1>
+      <StyledTest2></StyledTest2>
+    </>
+  );
+}
